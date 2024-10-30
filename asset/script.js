d5ecf2f8c9c1cf39dac6f83bf2cc1174cb813199
Yousif-Moh-Bashir <yousufmuhammud10@gmail.com>
2024-10-30 21:23:14 +0200

    uploade my website files

---
+++ asset/script.js
@@ -0,0 +1,119 @@
+
+// navigation bar function
+
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNaveMenu");
+    
+    if(menuBtn.className === "nav-menu"){
+        menuBtn.className += " responsive";
+    }
+    else{
+        menuBtn.className = "nav-menu";
+    }
+   
+}
+
+// add shadow on navigation bar while scrolling
+
+window.onscroll = function() {
+    headerShadow()
+}
+
+function headerShadow(){
+    const navHeader = document.getElementById("header");
+
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+    else{
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
+
+// Typing effect
+
+var typingEffect = new Typed(".typeText",{
+    strings : ["BackEnd","FrontEnd"],
+    loop : true,
+    typeSpeed : 80,
+    backDelay : 2000
+})
+
+// Scroll reveal animation
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+//  Home 
+
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social-icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+// projects 
+
+sr.reveal('.project-box',{interval: 200})
+
+// headings
+
+sr.reveal('.top-header',{})
+
+//  about info and contact info
+
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srLeft.reveal('.about-info',{interval: 100})
+srLeft.reveal('.contact-info',{interval: 100})
+
+// about skills and  form box
+
+const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+//  change active link
+
+const sections =  document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+
